03babeb4f9dad0741f8a6f626291fc927658f56f
mikkelam <mikkelarm@gmail.com>
2020-04-08 13:13:16 +0200

    Using new model, use new tf2 preprocess transforms

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,10 @@ FROM tensorflow/tensorflow:2.2.0rc2-gpu-jupyter
 COPY requirements.txt /tmp
 RUN pip install -r /tmp/requirements.txt
 
-COPY src/ /app/
+COPY . /app/
 WORKDIR /app
 
 
 RUN useradd -rm -d /home/docker -s /bin/bash -g root -G sudo -u 1000 docker
-
+RUN wget https://github.com/Callidior/keras-applications/releases/download/efficientnet/efficientnet-b3_weights_tf_dim_ordering_tf_kernels_autoaugment.hu
 USER docker
--- src/model.py
+++ src/model.py
@@ -2,9 +2,18 @@
 import tensorflow as tf
 import efficientnet.tfkeras as efn
 
+# from dataset import Dataset
+from src.dataset import Dataset
+
+
 def create_model(input_shape, output_shape) -> tf.keras.Model:
-    model = efn.EfficientNetB2(input_shape=input_shape, weights='noisy-student', include_top=False)
+    model = efn.EfficientNetB3(input_shape=input_shape, weights='noisy-student', include_top=False)
     model = tf.keras.Sequential([
+        Dataset.normalize_layer(input_shape),
+        tf.keras.layers.experimental.preprocessing.RandomFlip(),
+        tf.keras.layers.experimental.preprocessing.RandomTranslation(0.2, 0.2),
+        tf.keras.layers.experimental.preprocessing.RandomRotation(0.3),
+        tf.keras.layers.experimental.preprocessing.RandomContrast(0.35),
         model,
         tf.keras.layers.GlobalAveragePooling2D(),
         tf.keras.layers.Dense(output_shape)
